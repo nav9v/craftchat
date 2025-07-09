9319e7d6abeaaf3999649d73c5ebd6c12aabc824
navneet <navneetsharma2302@gmail.com>
2025-07-09 13:38:34 +0530

    relay update

--- src/routes/initGun.js
+++ src/routes/initGun.js
@@ -6,12 +6,16 @@ let peers;
 if (process.env.NODE_ENV === "development") {
   peers = ["http://localhost:8765/gun"];
 } else {
+
   peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
     "https://gun-manhattan.herokuapp.com/gun",
     "https://peer.wallie.io/gun",
     "https://gundb-relay-mlccl.ondigitalocean.app/gun",
-    "https://plankton-app-6qfp3.ondigitalocean.app",
+    "https://plankton-app-6qfp3.ondigitalocean.app/",
+    "https://gun.defucc.me/gun",
+    "https://a.talkflow.team/gun",
+    "https://talkflow.team/gun",
   ];
 }
 
